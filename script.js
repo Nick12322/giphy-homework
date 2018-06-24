282ff3a7f7b3e7f73ca0b546d0c7a0ae9833ac36
Nick Woodward <nickwoodward5@gmail.com>
2018-06-23 22:59:39 -0700

    added logic for creating buttons, displaying gifs when buttons clicked (currently breaks after adding a button to the list...

---
+++ script.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+
+var animals = ["Dogs", "Cats", "Birds", "Lions", "Tigers"];
+
+
+
+
+function renderButtons() {
+
+    $("#animals-list").empty();
+    for (var i = 0; i < animals.length; i++) {
+        $("#animals-list").append("<button id="+animals[i]+" class=rt>" + animals[i] + "</button>");
+    };
+};
+
+
+
+$("#add-animal").on("click", function (event) {
+    
+    var animalChosen = $("#animal-input").val();
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalChosen + "&api_key=P2zGaYjlUtwPSQZ3tbioUt0FAo6es3mA&limit=10";
+
+    $("#gifsGoHere").html("");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          var results = response.data;
+          for (var i = 0; i <= results.length; i++) {
+          $("#gifsGoHere").append("<img src="+results[i].images.fixed_height.url+"><br>");
+        }
+    });
+
+    event.preventDefault();
+    var userInput = $("#animal-input").val();
+    animals.push(userInput);
+    $("#animal-input").val("");
+    renderButtons();
+});
+
+renderButtons();
+
+$("#animals-list .rt").on("click", function() {
+    console.log(this.id);
+    var animalChosen = this.id;
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalChosen + "&api_key=P2zGaYjlUtwPSQZ3tbioUt0FAo6es3mA&limit=10";
+
+    $("#gifsGoHere").html("");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          var results = response.data;
+          for (var i = 0; i <= results.length; i++) {
+          $("#gifsGoHere").append("<img src="+results[i].images.fixed_height.url+"><br>");
+        };
+    });
+    
+
+});
+});
